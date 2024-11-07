023121c3ac632a8e4186eef7647e10e988854079
sami sabir-idrissi <samisabiridrissi@icloud.com>
2024-11-07 14:23:08 -0500

    added api to get bucket by name

--- tests/test_gcs_utils.py
+++
@@ -1,9 +0,0 @@
-from utils.gcs_utils import GCSClient
-
-
-def test_create_bucket():
-    gcs_client = GCSClient(project_id="rflkt-441001")
-    bucket = gcs_client.create_bucket(bucket_name="my-test-bucket-897629423")
-    print("Bucket creation attempted for: my-test-bucket")
-    assert bucket is not None
-    assert bucket.name == "my-test-bucket-897629423"
